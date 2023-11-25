c9025e367c97b68e0503a7131156ee26a22dcdef
최슬기 <choisk09@gmail.com>
2023-11-25 22:49:11 +0900

    created list box for SearchResultList

--- client/src/pages/SearchResultsPage.jsx
+++ client/src/pages/SearchResultsPage.jsx
@@ -1,5 +1,25 @@
+import SearchResultList from '../features/SearchResultList/SearchResultList';
+import Map from '../features/Map/Map';
+import { styled } from 'styled-components';
+
 const SearchResultsPage = () => {
-  return <div>SearchResultsPage</div>;
+  return <>
+    <Divider>
+      <LeftSide><SearchResultList /></LeftSide>
+      <RightSide><Map /></RightSide>
+
+    </Divider>
+
+  </>;
 };
 
 export default SearchResultsPage;
+
+const Divider = styled.div`
+display: flex;
+`
+const LeftSide = styled.div`
+flex:3`
+
+const RightSide = styled.div`
+flex:7`
